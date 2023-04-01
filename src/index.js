a66d8fb6cd8ddb175b9e15e203010a62f99b3ab9
olgabri <7774586@gmail.com>
2023-04-01 21:51:49 +0400

    if else event listener

--- src/index.js
+++ src/index.js
@@ -33,17 +33,56 @@ const person = {
     age: 25,
     isMarried: false
 };
-console.log(person.age); //=> get value from object by key
+// console.log(person.age); //=> get value from object by key
 // Arrays          0        1         2
 const fruits = ['apple', 'orange', 'cherry'];
-console.log(fruits[2]); //=> get element from array
+// console.log(fruits[2]); //=> get element from array
 // console.log(typeof fruits); //=> detect type of variable
 // type conversions
 let x = 1;
-console.log(typeof x);
+// console.log(typeof x)
 x = String(x); // '1'
-console.log(typeof x);
+// console.log(typeof x)
 x = Boolean(x); // true
-console.log(typeof x);
+// console.log(typeof x)
 x = Number(x); // 1
-console.log(typeof x);
+// console.log(typeof x)
+// falsy values: null, 0, undefined, false, ''
+// let someone = prompt("Please enter your name");
+// console.log(Boolean(someone))
+// if (someone) {
+//   alert("Hello " + someone + "! How are you today?");
+// } else {
+//   alert("You don't know?");
+// }
+const input = document.querySelector('.input');
+const buttonCancel = document.querySelector('.button-cancel');
+const buttonOk = document.querySelector('.button-ok');
+input === null || input === void 0 ? void 0 : input.addEventListener('keydown', (event) => {
+    if (event.code === 'Enter') {
+        proccessInput();
+    }
+});
+buttonCancel === null || buttonCancel === void 0 ? void 0 : buttonCancel.addEventListener('click', () => {
+    alert('cancel');
+});
+buttonOk === null || buttonOk === void 0 ? void 0 : buttonOk.addEventListener('click', proccessInput);
+function proccessInput() {
+    const response = input === null || input === void 0 ? void 0 : input.value;
+    console.log('ok clicked', response);
+    if (!response) {
+        alert('cancel');
+    }
+    else if (isNaN(+response)) {
+        alert('incorrect');
+    }
+    else if (+response > 0) {
+        alert("1");
+    }
+    else if (+response < 0) {
+        alert("-1");
+    }
+    else {
+        alert("0");
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -47,22 +47,65 @@ const person = {
     isMarried: false
 }
 
-console.log(person.age); //=> get value from object by key
+// console.log(person.age); //=> get value from object by key
 
 // Arrays          0        1         2
 const fruits = ['apple', 'orange', 'cherry']
 
-console.log(fruits[2]); //=> get element from array
+// console.log(fruits[2]); //=> get element from array
 
 // console.log(typeof fruits); //=> detect type of variable
 
 // type conversions
 let x: number | string | boolean = 1
-console.log(typeof x)
+// console.log(typeof x)
 x = String(x) // '1'
-console.log(typeof x)
+// console.log(typeof x)
 x = Boolean(x) // true
-console.log(typeof x)
+// console.log(typeof x)
 x = Number(x) // 1
-console.log(typeof x)
+// console.log(typeof x)
 
+// falsy values: null, 0, undefined, false, ''
+// let someone = prompt("Please enter your name");
+
+// console.log(Boolean(someone))
+
+// if (someone) {
+//   alert("Hello " + someone + "! How are you today?");
+// } else {
+//   alert("You don't know?");
+// }
+
+const input = document.querySelector<HTMLInputElement>('.input')
+const buttonCancel = document.querySelector<HTMLButtonElement>('.button-cancel')
+const buttonOk = document.querySelector<HTMLButtonElement>('.button-ok')
+
+input?.addEventListener('keydown', (event) => {
+    if (event.code === 'Enter') {
+        proccessInput()
+    }
+})
+
+buttonCancel?.addEventListener('click', () => {
+    alert('cancel');
+})
+
+buttonOk?.addEventListener('click', proccessInput)
+
+function proccessInput() {
+    const response = input?.value
+    console.log('ok clicked', response);
+
+    if (!response) {
+      alert ('cancel');
+    } else if (isNaN(+response)) {
+        alert('incorrect')
+    } else if (+response > 0) {
+      alert("1");
+    } else if (+response < 0) {
+      alert("-1");
+    } else {
+      alert("0");
+    }
+}
